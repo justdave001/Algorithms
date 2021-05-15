e8bd287eb7ddf34b2b113b88332c3a657b9928ee
justdave001 <81288192+justdave001@users.noreply.github.com>
2021-05-15 11:50:47 -0500

    Bubble Sort

---
+++ BubbleSort.py
@@ -0,0 +1,24 @@
+# Task: To sort an unsorted array using bubblesort
+
+
+def bubble_sort(inputArr):
+    #Declare the array as unsorted
+    isSorted = False
+
+    while not isSorted:
+"""
+      remains true until mismatch found then returns true again
+      when all numbers become sorted
+"""
+       isSorted = True
+       for i in range(len(inputArr)-1):
+         if inputArr[i] > inputArr[i+1]:
+             isSorted = False
+             #if number before is larger than number ahead, swap both numbers
+             inputArr[i], inputArr[i+1] = inputArr[i+1],inputArr[i]
+    return inputArr
+if __name__ == "__main__":
+     inputArr = list(map(int, input().rstrip().split()))
+     print(bubble_sort(inputArr))
+             
+
